24fb4b7fbdb9a944684f00455f6ecbdde6c66d5e
SeungBeom Kim <64590456+wewe231223@users.noreply.github.com>
2026-09-15 20:01:12 +0900

    GUID 버그 수정

--- Render/Panel/FPropertyEditorContext.cpp
+++ Render/Panel/FPropertyEditorContext.cpp
@@ -114,7 +114,7 @@ void FPropertyEditorContext::DrawAssetPicker(const char* Label, FAssetRegistry& 
         const FString Name = Asset->GetAssetName();
         ImGui::PushID(Asset);
         if (ImGui::Selectable(Name.c_str(), Asset == Current)) {
-            Setter(Registry.GetAsset(Asset->GetGuid()));
+            Setter(Registry.GetAsset(Name));
         }
         ImGui::PopID();
     }
